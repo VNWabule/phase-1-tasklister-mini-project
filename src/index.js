aaf84dc7c7c325f06c0be67c084122738ea6a740
VNWabule <vicwabule@gmail.com>
2025-03-18 14:32:56 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+document.addEventListener("DOMContentLoaded", function() {
+  let form = document.querySelector("form");
+  let tasklist = document.getElementById("tasks");
+
+  form.addEventListener("submit", function(event) {
+    event.preventDefault();
+
+    let inputField = document.getElementById("new-task-description");
+    let taskText = inputField.value.trim();
+
+    if (taskText !== "") {
+      let taskItem = document.createElement("li");
+      taskItem.textContent = taskText;
+
+      tasklist.appendChild(taskItem);
+      inputField.value = "";
+    }
+  });
 });
